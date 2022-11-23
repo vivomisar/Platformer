194fa5ec59a89bb749215f7d479614805692918c
vivomisar <vitiayu@gmail.com>
2022-11-23 19:14:38 +0300

    0

--- player/Player.gd
+++ player/Player.gd
@@ -1,22 +1,20 @@
 extends KinematicBody2D
 
 export var speed = 500
-export var gravity = 900
+export var gravity = 700
 export var jump_speed = 500
 
 var _velocity = Vector2.ZERO
-var _double_jump = 0
-var dash = false
+var _double_jump = 2
 
 func _physics_process(delta):
 	var direction = Input.get_action_strength("right") - Input.get_action_strength("left")
-	dash = Input.is_action_just_pressed("dash") #при одновременном нажатии кнопки направления с shift даёт рывок
-	_velocity.x = direction * speed + int(dash) * 8000 * direction
+	_velocity.x = direction * speed
 	_velocity.y += gravity * delta
 	if is_on_floor():
 		_double_jump = 2
 	if _double_jump and Input.is_action_just_pressed("jump"):
-		_velocity.y = -jump_speed + gravity*delta
+		_velocity.y = -jump_speed
 		_double_jump -= 1
 	_velocity = move_and_slide(_velocity, Vector2.UP)
 
