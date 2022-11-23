89d55db408f51af36371a56305724ecc85d8e12d
vivomisar <vitiayu@gmail.com>
2022-11-23 19:29:26 +0300

    2nd homework
    added dash, added tilemap

--- player/Player.gd
+++ player/Player.gd
@@ -6,10 +6,11 @@ export var jump_speed = 500
 
 var _velocity = Vector2.ZERO
 var _double_jump = 2
-
+var dash = false
 func _physics_process(delta):
 	var direction = Input.get_action_strength("right") - Input.get_action_strength("left")
-	_velocity.x = direction * speed
+	dash = Input.is_action_just_pressed("dash") # при нажатии shift персонаж делает рывок в сторону зажатой клавиши направления
+	_velocity.x = direction * speed + int(dash) * 8000 * direction
 	_velocity.y += gravity * delta
 	if is_on_floor():
 		_double_jump = 2
